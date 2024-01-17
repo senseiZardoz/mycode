317de81945fe6e50c91a7004bcfca052964c99c6
senseiZardoz <guillaumelb@icloud.com>
2024-01-17 16:54:19 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,31 @@
+# Terraform block
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+// provider block
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
+/*
+blabla
+blabla
+*/
